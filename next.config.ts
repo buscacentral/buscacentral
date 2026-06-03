5f12d7a1b2cdc28bff6d95a4136c45f7b1aa08bd
Peixoto <junior.bco@gmail.com>
2026-06-03 15:14:16 -0300

    feat: redirect /localizacao/whatsapp-link para /utilidades/whatsapp-link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/localizacao/whatsapp-link",
+        destination: "/utilidades/whatsapp-link",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
